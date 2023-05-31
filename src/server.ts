874b7bbb299491eccd54ffa81645bb1d60327e13
Edimar Insumbo <edhimar23@gmail.com>
2023-06-01 00:28:02 +0100

    Added cors and URL variables

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ import logger from "./infrestructure/config/logger";
 import { MiddllewareError } from './infrestructure/exceptions/ErrorHandling';
 import { initNantoiUser } from './domain/meta-inf/InitNantoiUser';
 import { PlataformConfig } from './infrestructure/config/plataform';
-
+import cors from 'cors';
 
 const swagger = require('./swagger');
 const myDataSource = require('./domain/meta-inf/data-source');
@@ -15,10 +15,32 @@ const myDataSource = require('./domain/meta-inf/data-source');
 // Configuração do Swagger
 
 
+
+
 myDataSource.initialize().then(() => {
 
     // create and setup express app
     const app = express()
+
+    var allowList = [ 
+        'http://localhost:3001',
+        'http://localhost:3002'
+        ];
+
+
+    var corsOptionsDelegate = (req, callback) => {
+
+    var corsOptions;
+    if (allowList.indexOf(req.header('Origin')) !== -1) {
+        corsOptions = { origin: true }
+    } else {
+        corsOptions = { origin: false }
+    }
+        callback(null, corsOptions);
+    }
+
+
+    app.use(cors(corsOptionsDelegate));
 
     app.use(express.json())
 
@@ -30,10 +52,10 @@ myDataSource.initialize().then(() => {
 
     swagger(app);
 
-    app.listen(PlataformConfig.server.port)
+    app.listen(PlataformConfig.server.port);
 
     console.info("Data Source has been initialized!")
-    logger.info("Data Source has been initialized!")
+    logger.info("Data Source has been initialized!");
 
 }).catch((error) => {
     logger.error("Error during Data Source initialization:" + "", error)
